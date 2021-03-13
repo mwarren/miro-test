b7c5e41e5a3477ee8c8e98c8e40038554e47115d
Mark Warren <mwarren@akamai.com>
2021-03-12 20:26:01 -0500

    another

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,39 +1,39 @@
 function showStatistics(selection) {
   setPointsTo(0);
-  const storyPoints = countStoryPoints(selection);
-  console.log(`found ${storyPoints} story points in the selected widgets`);
-  setPointsTo(storyPoints);
+  const storyPoints = countStoryPoints(selection).then((storyPoints) => {
+    console.log(`found ${storyPoints} story points in the selected widgets`);
+    setPointsTo(storyPoints);
+  };;
 }
 
 function setPointsTo(x) {
-  console.log('updating story points');
   document.getElementById('counter').innerHtml = `${x}`;
 }
 
-function countStoryPoints(selection) {
+async function countStoryPoints(selection) {
   const re = /\((?<points>\d+)\)/g;
 
   console.log(`examining ${selection.length} widgets`);
   var total = 0;
-  selection.forEach((sel) => {
-    console.log(sel);
-    miro.board.widgets.get({id: sel.id}).then((widgets) => {
-      const widget = widgets[0];
-      console.log(widget);
-      if (widget.title) {
-        let match = re.exec(widget.title);
-        if (match !== null) {
-          total += parseInt(match.groups.points, 10);
-        }
-        else {
-          console.log('regex didnt match the element title');
-        }
+
+  for (const sel of selection) {
+    const widgets = await miro.board.widgets.get({id: sel.id})
+    const widget = widgets[0];
+    console.log(widget);
+
+    if (widget.title) {
+      let match = re.exec(widget.title);
+      if (match !== null) {
+        total += parseInt(match.groups.points, 10);
       }
       else {
-        console.log('element has no title');
+        console.log('regex didnt match the element title');
       }
-    });
-  });
+    }
+    else {
+      console.log('element has no title');
+    }
+  }
 
   return total;
 }
