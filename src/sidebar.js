da3967c29f10c32c78ec5f2150d67aeccdb88826
Mark Warren <mwarren@akamai.com>
2021-03-12 19:31:38 -0500

    initial commit

---
+++ src/sidebar.js
@@ -0,0 +1,46 @@
+function showStatistics(selection) {
+  clear();
+  const storyPoints = countStoryPoints(selection);
+  getContainer().appendChild(createTotal(storyPoints));
+}
+
+function clear() {
+  const elements = getContainer().getElementsByClassName('story-point-total')
+  for (let i = 0; i < elements.length; i++) {
+    elements.item(i).remove()
+  }
+}
+
+function getContainer() {
+  return document.getElementById('story-point-table');
+}
+
+function createTotal(storyPoints) {
+  const statView = document.createElement('p');
+  statView.className = 'story-point-total';
+  statView.innerHtml = `<span>${storyPoints}</span>`;
+  return statView;
+}
+
+function countStoryPoints(selection) {
+  const re = /^.*\((?<points>\d+)\)\s*$/mg;
+
+  var total = 0;
+  selection.forEach((item) => {
+    if (item.title) {
+      let match = re.exec(item.title);
+      if (match !== null) {
+        total += parseInt(match.groups.points, 10);
+      }
+    }
+  });
+
+  return total;
+}
+
+miro.onReady(() => {
+  miro.addListener('SELECTION_UPDATED', (e) => {
+    showStatistics(e.data)
+  })
+  miro.board.selection.get().then(showStatistics)
+});
