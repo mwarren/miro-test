ebe65c20b7d50b7079169cbf6c22130f1f30a0b8
Mark Warren <mwarren@akamai.com>
2021-03-12 19:52:17 -0500

    logging

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,16 +1,19 @@
 function showStatistics(selection) {
   setPointsTo(0);
   const storyPoints = countStoryPoints(selection);
+  console.log(`found ${storyPoints} story points in the selected widgets`);
   setPointsTo(storyPoints);
 }
 
 function setPointsTo(x) {
+  console.log('updating story points');
   document.getElementById('counter').innerHtml = `${x}`;
 }
 
 function countStoryPoints(selection) {
   const re = /^.*\((?<points>\d+)\)\s*$/mg;
 
+  console.log(`examining ${selection.length} widgets`);
   var total = 0;
   selection.forEach((item) => {
     if (item.title) {
@@ -18,6 +21,12 @@ function countStoryPoints(selection) {
       if (match !== null) {
         total += parseInt(match.groups.points, 10);
       }
+      else {
+        console.log('regex didnt match the element title');
+      }
+    }
+    else {
+      console.log('element has no title');
     }
   });
 
